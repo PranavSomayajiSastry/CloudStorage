75239140b7884449208f3683b31d3175e7c23d31
PranavSomayajiSastry <58330938+PranavSomayajiSastry@users.noreply.github.com>
2020-07-30 21:25:46 +0530

    Update cloudStorage.py

--- CloudStorage/cloudStorage.py
+++ CloudStorage/cloudStorage.py
@@ -1,21 +1,29 @@
+import os
 import dropbox
+from dropbox.files import WriteMode
 
 class TransferData:
-    def __init__(self, access_token):
-        self.access_token = access_token
+    def __init__(self,access_token):
+        self.access_token =  access_token
+
     def upload_file(self, file_from, file_to):
         dbx = dropbox.Dropbox(self.access_token)
-        f = open(file_from, 'rb')
-        dbx.files_upload(f.read(), file_to)
 
+        for root, dirs, files in os.walk(file_from):
+
+            for filename in files:
+                local_path = os.path.join(root, filename)
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, relative_path)  
+                                 
 def main():
-    access_token = ''
+    access_token = 'riFu6Ybhc9AAAAAAAAAAHWkfE9AiGyD6n4254tOxesw7ShRjGjFhrjhRVa3NX3mx'
         #access token to be coppied from https://www.dropbox.com/developers/apps/
     transferData = TransferData(access_token)
 
-    file_from = input("Enter the file path to transfer: ")
-    file_to = input("Enter the full path to upload to dropbox: ")  
-        # API v2
-    transferData.upload_file(file_from, file_to)
-    print("!!File(s)/ Folder(s) have been Successfully Uploaded!!")
+    file_from = str(input("Enter The File Path To Transfer: "))
+    file_to = input("Enter The Full Path To Upload To Dropbox: ")
+    # API v2
+    transferData.upload_file(file_from,file_to)
+    print("!!The File(s)/ Folder(s) Has/ Have Been Moved!!")
 main()
